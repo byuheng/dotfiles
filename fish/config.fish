585a445464ad4d06ca99fa9e0a6f0cbd915d8f68
byuheng <141770040+byuheng@users.noreply.github.com>
2025-12-28 21:34:05 -0800

    Added mpv + yt-dlp fish configs

--- fish/config.fish
+++ fish/config.fish
@@ -4,3 +4,24 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 # potentially disabling fastfetch
 function fish_greeting
 end
+
+# mpv + ytdl: high-quality audio-only premium YT music via Zen/Firefox symlink
+# Usage: music "URL" or music "ytsearch:Song Name"
+function music
+    mpv --video=no \
+        --ytdl-format="bestaudio/best" \
+        --ytdl-raw-options="cookies-from-browser=firefox,ignore-errors=" \
+        --msg-level=all=status \
+        --shuffle \
+        --term-osd-bar \
+        $argv
+end
+
+# Usage: watch "URL"
+function watch
+    mpv --ytdl-format="bestvideo[height<=1080]+bestaudio/best" \
+        --ytdl-raw-options="cookies-from-browser=firefox" \
+        --save-position-on-quit \
+        --hwdec=auto \
+        $argv
+end
