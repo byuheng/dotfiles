a3e2468674730438dcf7b1f552234c4b27b6384e
Heng Ky <141770040+byuheng@users.noreply.github.com>
2026-08-19 16:41:28 -0700

    update: fish and yazi config

--- fish/config.fish
+++ fish/config.fish
@@ -1,67 +1,147 @@
-source /usr/share/cachyos-fish-config/cachyos-config.fish
-
-#Ensure system-installed fisher is loaded
-if not functions -q fisher
-    source /usr/share/fish/vendor_functions.d/fisher.fish
+# Source CachyOS base config
+if test -f /usr/share/cachyos-fish-config/cachyos-config.fish
+    source /usr/share/cachyos-fish-config/cachyos-config.fish
 end
 
-# overwrite greeting
-# potentially disabling fastfetch
+# Auto-launch Sway on tty1 login with NVIDIA flags
+if status is-login
+    if test -z "$WAYLAND_DISPLAY" -a "$XDG_VTNR" = 1
+        set -gx LIBVA_DRIVER_NAME nvidia
+        set -gx XDG_SESSION_TYPE wayland
+        set -gx __GLX_VENDOR_LIBRARY_NAME nvidia
+        set -gx GBM_BACKEND nvidia-drm
+        set -gx NVD_BACKEND direct
+        set -gx ELECTRON_OZONE_PLATFORM_HINT auto
+        set -gx SWAY_UNSUPPORTED_GPU 1
+
+        exec sway --unsupported-gpu
+    end
+end
+
+# Disable fastfetch/welcome greeting
 function fish_greeting
 end
 
-# Theme
-set --global fish_color_autosuggestion 504945
-set --global fish_color_cancel -r
-set --global fish_color_command a9b665
-set --global fish_color_comment 928374
-set --global fish_color_cwd green
-set --global fish_color_cwd_root red
-set --global fish_color_end ea6962
-set --global fish_color_error ea6962
-set --global fish_color_escape 89b482
-set --global fish_color_history_current --bold
-set --global fish_color_host normal
-set --global fish_color_host_remote yellow
-set --global fish_color_match --background=3c3836
-set --global fish_color_normal d4be98
-set --global fish_color_operator 89b482
-set --global fish_color_param d4be98
-set --global fish_color_quote d8a657
-set --global fish_color_redirection d3869b
-set --global fish_color_search_match --background=3c3836
-set --global fish_color_selection --background=3c3836
-set --global fish_color_status red
-set --global fish_color_user brgreen
-set --global fish_color_valid_path 89b482
-set --global fish_pager_color_background
-set --global fish_pager_color_completion d4be98
-set --global fish_pager_color_description 928374
-set --global fish_pager_color_prefix d4be98 --bold --underline
-set --global fish_pager_color_progress 504945 --background=d4be98
-set --global fish_pager_color_secondary_background
-set --global fish_pager_color_secondary_completion
-set --global fish_pager_color_secondary_description
-set --global fish_pager_color_secondary_prefix
-set --global fish_pager_color_selected_background -r
-set --global fish_pager_color_selected_completion
-set --global fish_pager_color_selected_description
-set --global fish_pager_color_selected_prefix
-
+# Clear custom keybindings override if unwanted
 set --erase --universal fish_key_bindings
 
-# Pure
-set -g pure_enable_single_line_prompt false
-set -g pure_show_user_host false
+# Interactive Shell Settings (Theme & Prompt Overrides)
+if status is-interactive
 
-# Pure Git colors
-set -g pure_color_git_branch 504945
-set -g pure_color_git_dirty ea6962
-set -g pure_color_git_stash d8a657
-set -g pure_color_git_unpulled_commits 89b482
-set -g pure_color_git_unpushed_commits 89b482
+    # --- Gruvbox Syntax Highlighting Colors ---
+    set -g fish_color_autosuggestion 504945
+    set -g fish_color_cancel -r
+    set -g fish_color_command a9b665
+    set -g fish_color_comment 928374
+    set -g fish_color_cwd green
+    set -g fish_color_cwd_root red
+    set -g fish_color_end ea6962
+    set -g fish_color_error ea6962
+    set -g fish_color_escape 89b482
+    set -g fish_color_history_current --bold
+    set -g fish_color_host normal
+    set -g fish_color_host_remote yellow
+    set -g fish_color_match --background=3c3836
+    set -g fish_color_normal d4be98
+    set -g fish_color_operator 89b482
+    set -g fish_color_param d4be98
+    set -g fish_color_quote d8a657
+    set -g fish_color_redirection d3869b
+    set -g fish_color_search_match --background=3c3836
+    set -g fish_color_selection --background=3c3836
+    set -g fish_color_status red
+    set -g fish_color_user brgreen
+    set -g fish_color_valid_path 89b482
 
+    # --- Pager Colors ---
+    set -g fish_pager_color_background
+    set -g fish_pager_color_completion d4be98
+    set -g fish_pager_color_description 928374
+    set -g fish_pager_color_prefix d4be98 --bold --underline
+    set -g fish_pager_color_progress 504945 --background=d4be98
+    set -g fish_pager_color_secondary_background
+    set -g fish_pager_color_secondary_completion
+    set -g fish_pager_color_secondary_description
+    set -g fish_pager_color_secondary_prefix
+    set -g fish_pager_color_selected_background -r
+    set -g fish_pager_color_selected_completion
+    set -g fish_pager_color_selected_description
+    set -g fish_pager_color_selected_prefix
 
-# mpv + ytdl: high-quality audio-only premium YT music via Zen/Firefox symlink
-# Usage: music "URL" or music "ytsearch:Song Name"
+    # --- Pure Prompt Overrides ---
+    set -g pure_enable_single_line_prompt false
+    set -g pure_show_user_host false
+    set -g pure_color_current_directory a9b665
+    set -g pure_color_primary d4be98
+    set -g pure_color_git_branch 504945
+    set -g pure_color_git_dirty ea6962
+    set -g pure_color_git_stash d8a657
+    set -g pure_color_git_unpulled_commits 89b482
+    set -g pure_color_git_unpushed_commits 89b482
 
+end
+
+# source /usr/share/cachyos-fish-config/cachyos-config.fish
+#
+# # Sway auto-launch on tty1 with NVIDIA flags
+# if status is-login
+#     if test -z "$WAYLAND_DISPLAY" -a "$XDG_VTNR" = 1
+#         set -gx LIBVA_DRIVER_NAME nvidia
+#         set -gx XDG_SESSION_TYPE wayland
+#         set -gx __GLX_VENDOR_LIBRARY_NAME nvidia
+#         set -gx GBM_BACKEND nvidia-drm
+#         set -gx NVD_BACKEND direct
+#         set -gx ELECTRON_OZONE_PLATFORM_HINT auto
+#         set -gx SWAY_UNSUPPORTED_GPU 1
+#
+#         exec sway --unsupported-gpu
+#     end
+# end
+#
+# # Disable fastfetch greeting
+# function fish_greeting
+# end
+#
+# set --erase --universal fish_key_bindings
+#
+# # Interactive Shell Settings (Theme & Prompt)
+# if status is-interactive
+#     # Theme
+#     set -g fish_color_autosuggestion 504945
+#     set -g fish_color_cancel -r
+#     set -g fish_color_command a9b665
+#     set -g fish_color_comment 928374
+#     set -g fish_color_cwd green
+#     set -g fish_color_cwd_root red
+#     set -g fish_color_end ea6962
+#     set -g fish_color_error ea6962
+#     set -g fish_color_escape 89b482
+#     set -g fish_color_history_current --bold
+#     set -g fish_color_host normal
+#     set -g fish_color_host_remote yellow
+#     set -g fish_color_match --background=3c3836
+#     set -g fish_color_normal d4be98
+#     set -g fish_color_operator 89b482
+#     set -g fish_color_param d4be98
+#     set -g fish_color_quote d8a657
+#     set -g fish_color_redirection d3869b
+#     set -g fish_color_search_match --background=3c3836
+#     set -g fish_color_selection --background=3c3836
+#     set -g fish_color_status red
+#     set -g fish_color_user brgreen
+#     set -g fish_color_valid_path 89b482
+#     set -g fish_pager_color_completion d4be98
+#     set -g fish_pager_color_description 928374
+#     set -g fish_pager_color_prefix d4be98 --bold --underline
+#     set -g fish_pager_color_progress 504945 --background=d4be98
+#     set -g fish_pager_color_selected_background -r
+#
+#     # Pure Prompt
+#     set -g pure_enable_single_line_prompt false
+#     set -g pure_show_user_host false
+#     set -g pure_color_git_branch 504945
+#     set -g pure_color_git_dirty ea6962
+#     set -g pure_color_git_stash d8a657
+#     set -g pure_color_git_unpulled_commits 89b482
+#     set -g pure_color_git_unpushed_commits 89b482
+# end
